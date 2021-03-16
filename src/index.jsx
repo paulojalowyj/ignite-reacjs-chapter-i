2998f5985bdc66a7aa950bc41469e26935281b3b
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-03-16 20:16:11 -0300

    Servindo HTML Estático

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+import { App } from './App'
+
+/**O render recebe dois parâmetros, oque eu quero exibir em tela e o segundo em qual elemento eu quero renderizar */
+render(<App />, document.getElementById('root'))
